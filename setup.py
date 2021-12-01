c472a70b0df269da703ed4547cd9e7e9b4deb33e
AndrewSpitia <germandres20@hotmail.es>
2021-11-30 23:32:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="packages",
+    name="packages_module",
     version="0.1",
-    packages=['packages'],
+    packages=['packages_module'],
     install_requires=['requests','beautifulsoup4','boto3']
 )
